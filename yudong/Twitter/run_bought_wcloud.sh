665f42884ece2f32328d44fb957151e5d535de4e
qdwwqxwn <ydtian@gmail.com>
2016-03-08 19:38:21 -0500

    cron driver

---
+++ yudong/Twitter/run_bought_wcloud.sh
@@ -0,0 +1,11 @@
+#! /bin/bash 
+
+
+cd /home/yudong/data/Twitter
+/usr/bin/Rscript  bought_wcloud.R
+
+filename=`date +bought/%Y-%m-%d.png`
+scp $filename ec2-user@cloud.big3k.org:/data/www/free/twitter_daily/
+
+
+
