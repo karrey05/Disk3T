ba29bace85ac3a53698afeb8dc376a518893ca23
qdwwqxwn <ydtian@gmail.com>
2016-04-05 23:33:08 -0400

    Changed the colors a bit to differentiate from NO2.

--- yudong/OMI/SO2/vis_djf.R
+++ yudong/OMI/SO2/vis_djf.R
@@ -27,7 +27,7 @@ png("so2_cities_2005-2016.png", width=1000, height=700, bg='white')
 par(mai=c(1.02, 1.02, 0.82, 0.42), xpd=NA) 
 plot.stacked(2005:2016, df[2:(nc+1)], xlab='Year', xaxt='n', yaxt='n', xlim=c(2004, 2016),   
          lwd = 2, bty='n', border='white', cex.lab=1.5, main='SO2 Density', 
-        col=rgb(1:nc*(155/nc), 128, 128, 200, maxColorValue = 255) ) 
+        col=rgb(1:nc*(255/nc), 128, 128, 200, maxColorValue = 255) ) 
 axis(1, at=2005:2016, cex.axis=1.5)
 
 stack_y = rep(0, nc) 
